816a332beeee20c1f10f9f8b87a081e02453de3a
pikokr <parangee9706@gmail.com>
2020-12-28 16:24:41 +0900

    feat: bow cooldown and reset cooldown on arrow hot

--- customItem/src/main/kotlin/com/github/pikokr/mc/customitems/events/FollowingBow.kt
+++ customItem/src/main/kotlin/com/github/pikokr/mc/customitems/events/FollowingBow.kt
@@ -1,9 +1,11 @@
 package com.github.pikokr.mc.customitems.events
 
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.ProjectileHitEvent
 import org.bukkit.event.entity.ProjectileLaunchEvent
 
 class FollowingBow : Listener {
@@ -15,7 +17,18 @@ class FollowingBow : Listener {
             val p = e.entity.shooter as Player
             if (p.inventory.itemInMainHand.itemMeta?.lore?.last() == "CUSTOM__follow_bow") {
                 e.entity.addPassenger(p)
+                p.setCooldown(Material.BOW, 60)
             }
+        }
+    }
+
+    @EventHandler
+    fun onProjectileHit(e: ProjectileHitEvent) {
+        if (e.entityType == EntityType.ARROW) {
+            e.entity.shooter ?: return
+            if (e.entity.shooter !is Player) return
+            val p = e.entity.shooter as Player
+            p.setCooldown(Material.BOW, 0)
         }
     }
 }
