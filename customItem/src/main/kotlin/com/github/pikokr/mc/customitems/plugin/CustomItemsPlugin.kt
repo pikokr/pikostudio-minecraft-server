49a739434f06fadcaf0bde4fcf69835fdf96a623
pikokr <parangee9706@gmail.com>
2020-12-29 14:58:30 +0900

    backup

--- customItem/src/main/kotlin/com/github/pikokr/mc/customitems/plugin/CustomItemsPlugin.kt
+++ customItem/src/main/kotlin/com/github/pikokr/mc/customitems/plugin/CustomItemsPlugin.kt
@@ -4,6 +4,10 @@ import com.github.pikokr.mc.customitems.command.initCommands
 import com.github.pikokr.mc.customitems.events.CustomItemListener
 import com.github.pikokr.mc.customitems.events.FollowingBow
 import com.github.pikokr.mc.customitems.items.initItems
+import com.github.pikokr.mc.customitems.registry.CustomItemsRegistry
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ShapedRecipe
 import org.bukkit.plugin.java.JavaPlugin
 
 class CustomItemsPlugin : JavaPlugin() {
@@ -12,5 +16,17 @@ class CustomItemsPlugin : JavaPlugin() {
         initItems(this)
         server.pluginManager.registerEvents(CustomItemListener(this), this)
         server.pluginManager.registerEvents(FollowingBow(), this)
+        registerRecipes()
+    }
+
+    private fun registerRecipes() {
+        val bow = CustomItemsRegistry.items["follow_bow"]
+        bow?.let {
+            val recipe = ShapedRecipe(NamespacedKey(this, "follow_bow"), bow.asOne())
+            recipe.shape("***", "*%*", "***")
+            recipe.setIngredient('*', Material.ENDER_PEARL)
+            recipe.setIngredient('%', Material.BOW)
+            server.addRecipe(recipe)
+        }
     }
 }
